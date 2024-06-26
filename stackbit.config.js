2ab289358b4ba7138c012d476dbbf4986366d854
Isa Robertini <robertiniisa@gmail.com>
2024-06-26 13:53:29 +0200

    devcommand

--- stackbit.config.js
+++ stackbit.config.js
@@ -2,4 +2,5 @@ export default {
     installCommand: 'npm install',
     postGitCloneCommand: 'node ./scripts/git-setup.js',
     postInstallCommand: 'npm run cleanup',
+    devCommand: 'npm run dev', // Adjust this command according to your project setup
 };
